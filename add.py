d6437ec26be4c891f86f5ad30f8983c68a86331d
Muhammad Areeb Khan <103059817+Mreeb@users.noreply.github.com>
2024-06-23 13:28:28 +0500

    Update add.py

--- add.py
+++ add.py
@@ -1,8 +1,11 @@
 def add(a, b):
     return a + b
 
-def sub(a,c):
-    return a-b
+
+def sub(a, c):
+    return a - b
+
+
 if __name__ == "__main__":
     print(add(1, 2))
-    print(add(2,1))
+    print(sub(2, 1))
